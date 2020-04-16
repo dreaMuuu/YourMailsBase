1907dc7d3b518b775cbb391552aaf828c1284611
dreaMuuu <piotr.filipek@protonmail.com>
2020-04-17 00:12:21 +0200

    Add MailsBase class

---
+++ src/MailsBase.py
@@ -0,0 +1,11 @@
+class MailsBase:
+
+    def __init__(self, path):
+        self.path = path
+        self.mails = []
+
+    def loadEmailAddresses(self):
+        pass
+
+    def saveBase(self):
+        pass
