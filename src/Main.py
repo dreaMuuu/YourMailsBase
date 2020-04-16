84b4175ec3e56f63750ec67ab48f9065ce270712
dreaMuuu <piotr.filipek@protonmail.com>
2020-04-17 00:33:43 +0200

    Started with Main.py

---
+++ src/Main.py
@@ -0,0 +1,14 @@
+currentBasePath = ""
+newBasePath = ""
+
+print("Before using app, be sure your emailsList is in txt file in right place (how to prepare emails list you can read in readme")
+print("Pass name of file with current base with extension: ")
+currentBasePath = input(">")
+print("Are you sure you want to use " + currentBasePath + " file? Y/N")
+# decision
+print("Pass name of new file containing Emails Base with extension")
+newBasePath = input(">")
+print("Do you want to delete clones? Y/N")
+# decision
+print("Do you want to delete incorrect email addresses? Y/N")
+# decision
