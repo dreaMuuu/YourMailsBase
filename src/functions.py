73fc0b52900095b923eb405ad77b08064dfe17db
dreaMuuu <piotr.filipek@protonmail.com>
2020-04-17 18:09:55 +0200

    Update

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,13 @@
-def deleteClones(currentEmails):
+from email_validator import validate_email, EmailNotValidError
+
+
+def returnWithoutClones(currentEmails):
     newEmails = []
     for index in currentEmails:
         if not (currentEmails[index] in newEmails):
             newEmails.append(currentEmails[index])
     return newEmails
+
+
+def returnOnlyValid(currentEmails):
+    newEmails = []
