554a552c3169d20904b907da35715393a32d3e3b
dreaMuuu <piotr.filipek@protonmail.com>
2020-04-18 10:08:33 +0200

    Update main

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,6 @@
 from MailsBase import MailsBase
 from functions import returnWithoutClones, returnOnlyValid, yesOrNo
+import sys
 
 currentBasePath = ""
 newBasePath = ""
@@ -8,21 +9,23 @@ newEmails = []
 print("Before using app, be sure your emailsList is in txt file in right place (how to prepare emails list you can read in readme")
 print("Pass name of file with current base with extension: ")
 currentBasePath = input(">")
-print("Are you sure you want to use " + currentBasePath + " file? Y/N")
-# decision
-actualBase = MailsBase(currentBasePath)
-actualBase.loadEmailAddresses()
-newEmails = actualBase.getMails()
+if yesOrNo("Are you sure you want to use " + currentBasePath + " file?"):
+    actualBase = MailsBase(currentBasePath)
+    actualBase.loadEmailAddresses()
+    newEmails = actualBase.getMails()
+else:
+    print("Ok, so i am closing program")
+    sys.exit()
 print("Pass name of new file containing Emails Base with extension")
 newBasePath = input(">")
-print("Do you want to delete clones? Y/N")
-# decision
-newEmails = returnWithoutClones(newEmails)
-print(newEmails)
-print("Do you want to delete incorrect email addresses? Y/N")
-# decision
-newEmails = returnOnlyValid(newEmails)
-print(newEmails)
+if yesOrNo("Do you want to delete clones?"):
+    newEmails = returnWithoutClones(newEmails)
+else:
+    print("Ok, so i am not deleting clones")
+if yesOrNo("Do you want to delete incorrect email addresses?"):
+    newEmails = returnOnlyValid(newEmails)
+else:
+    print("Ok, so i am not deleting incorrect email addresses.")
 newBase = MailsBase(newBasePath)
 newBase.pushMails(newEmails)
 newBase.saveBase()
