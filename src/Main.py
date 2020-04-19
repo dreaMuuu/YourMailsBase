755efc77878ad8d24d6b578cb0cf03721efff5ee
dreaMuuu <piotr.filipek@protonmail.com>
2020-04-19 12:16:22 +0200

    Update

--- src/Main.py
+++ src/Main.py
@@ -8,27 +8,31 @@ currentBasePath = ""
 newBasePath = ""
 newEmails = []
 
-print("Before using app make sure that your base is preapred correctly")
-print("Choose .txt file with your base: ")
-time.sleep(2)
+print("> YourMailsBase - version 1.0.0 - copyrights @ Piotr Filipek")
+time.sleep(1)
+print("> Before using app make sure that your base is preapred correctly")
+print("> Choose .txt file with your base: ")
+input("> Press any key to open files window...")
 currentBasePath = openFile()
-if yesOrNo("Are you sure you want to use " + currentBasePath + " file?"):
+if yesOrNo("> Are you sure you want to use " + currentBasePath + " file?"):
     actualBase = MailsBase(currentBasePath)
     actualBase.loadEmailAddresses()
     newEmails = actualBase.getMails()
 else:
-    print("Ok, so i am closing program")
+    print("> Ok, so i am closing program")
     sys.exit()
-if yesOrNo("Do you want to delete clones?"):
+if yesOrNo("> Do you want to delete clones?"):
     newEmails = returnWithoutClones(newEmails)
 else:
-    print("Ok, so i am not deleting clones")
-if yesOrNo("Do you want to delete incorrect email addresses?"):
+    print("> Ok, so i am not deleting clones")
+if yesOrNo("> Do you want to delete incorrect email addresses?"):
     newEmails = returnOnlyValid(newEmails)
 else:
-    print("Ok, so i am not deleting incorrect email addresses.")
+    print("> Ok, so i am not deleting incorrect email addresses.")
+print("> Where should I save your base?")
+input("> Press any key to open files window...")
 newBasePath = saveFile()
 newBase = MailsBase(newBasePath)
 newBase.pushMails(newEmails)
 newBase.saveBase()
-print("Your email addresses base is actual. You will find it as: " + newBasePath)
+print("> Your email addresses base is actual. You will find it here: " + newBasePath)
